6146441ffa9c622f873caff211049a1a0ce0da94
Jairo Nonato Júnior <53584770+jaijunior@users.noreply.github.com>
2022-06-08 20:22:02 -0300

    Portuguese translate first commit

---
+++ translations/ukui-menu_pt_BR.ts
@@ -0,0 +1,292 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>FunctionClassifyButton</name>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="268"/>
+        <source>Mobile</source>
+        <translation type="unfinished">Móvel</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="271"/>
+        <source>Internet</source>
+        <translation type="unfinished">Internet</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="274"/>
+        <source>Social</source>
+        <translation type="unfinished">Rede Social</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="277"/>
+        <source>Video</source>
+        <translation type="unfinished">Vídeo</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="280"/>
+        <source>Development</source>
+        <translation type="unfinished">Desenvolvimento</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="283"/>
+        <source>Image</source>
+        <translation type="unfinished">Imagem</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="286"/>
+        <source>Game</source>
+        <translation type="unfinished">Jogos</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="289"/>
+        <source>Office</source>
+        <translation type="unfinished">Escritorio</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="292"/>
+        <source>Education</source>
+        <translation type="unfinished">Educação</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="295"/>
+        <source>System</source>
+        <translation type="unfinished">Sistema</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/functionclassifybutton.cpp" line="298"/>
+        <source>Others</source>
+        <translation type="unfinished">Outros</translation>
+    </message>
+</context>
+<context>
+    <name>FunctionWidget</name>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="87"/>
+        <source>Mobile</source>
+        <translation type="unfinished">Móvel</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="93"/>
+        <source>Internet</source>
+        <translation type="unfinished">Internet</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="99"/>
+        <source>Social</source>
+        <translation type="unfinished">Rede Social</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="105"/>
+        <source>Video</source>
+        <translation type="unfinished">Vídeo</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="111"/>
+        <source>Development</source>
+        <translation type="unfinished">Desenvolvimento</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="117"/>
+        <source>Image</source>
+        <translation type="unfinished">Imagem</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="123"/>
+        <source>Game</source>
+        <translation type="unfinished">Jogos</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="129"/>
+        <source>Office</source>
+        <translation type="unfinished">Escritorio</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="135"/>
+        <source>Education</source>
+        <translation type="unfinished">Educação</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="142"/>
+        <source>System</source>
+        <translation type="unfinished">Sistema</translation>
+    </message>
+    <message>
+        <location filename="../src/FunctionWidget/functionwidget.cpp" line="148"/>
+        <source>Others</source>
+        <translation type="unfinished">Outros</translation>
+    </message>
+</context>
+<context>
+    <name>MainViewWidget</name>
+    <message>
+        <location filename="../src/MainViewWidget/mainviewwidget.cpp" line="226"/>
+        <source>Search</source>
+        <translation type="unfinished">Pesquisar</translation>
+    </message>
+</context>
+<context>
+    <name>RightClickMenu</name>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="216"/>
+        <source>Pin to all</source>
+        <translation type="unfinished">Fixar em todos</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="219"/>
+        <source>Unpin from all</source>
+        <translation type="unfinished">Desafixar de todos</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="228"/>
+        <source>Pin to taskbar</source>
+        <translation type="unfinished">Fixar a barra de tarefas</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="231"/>
+        <source>Unpin from taskbar</source>
+        <translation type="unfinished">Desafixar da barra de tarefas</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="234"/>
+        <source>Add to desktop shortcuts</source>
+        <translation type="unfinished">Adicionar aos atalhos da área de trabalho</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="245"/>
+        <source>Uninstall</source>
+        <translation type="unfinished">Desinstalar</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="259"/>
+        <source>Sleep</source>
+        <translation type="unfinished">Suspender</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="261"/>
+        <source>Log Out</source>
+        <translation type="unfinished">Sair</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="263"/>
+        <source>Restart</source>
+        <translation type="unfinished">Reiniciar</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="265"/>
+        <source>Power Off</source>
+        <translation type="unfinished">Desligar</translation>
+    </message>
+    <message>
+        <location filename="../src/RightClickMenu/rightclickmenu.cpp" line="292"/>
+        <source>Personalize this list</source>
+        <translation type="unfinished">Personalizar essa lista</translation>
+    </message>
+</context>
+<context>
+    <name>SideBarWidget</name>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="95"/>
+        <source>All</source>
+        <translation type="unfinished">Todos</translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="97"/>
+        <source>Letter</source>
+        <translation type="unfinished">E-mail</translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="99"/>
+        <source>Function</source>
+        <translation type="unfinished">Função</translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="113"/>
+        <source>Trash</source>
+        <translation type="unfinished">Lixeira</translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="115"/>
+        <source>Computer</source>
+        <translation type="unfinished">Computador</translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="469"/>
+        <source>Max</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="111"/>
+        <source>Personal</source>
+        <translation type="unfinished">Pessoal</translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="117"/>
+        <source>Settings</source>
+        <translation type="unfinished">Configurações</translation>
+    </message>
+    <message>
+        <location filename="../src/SideBarWidget/sidebarwidget.cpp" line="119"/>
+        <source>Power</source>
+        <translation type="unfinished">Ligar</translation>
+    </message>
+</context>
+<context>
+    <name>SplitBarFrame</name>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="79"/>
+        <source>Mobile</source>
+        <translation type="unfinished">Móvel</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="82"/>
+        <source>Internet</source>
+        <translation type="unfinished">Internet</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="85"/>
+        <source>Social</source>
+        <translation type="unfinished">Rede Social</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="88"/>
+        <source>Video</source>
+        <translation type="unfinished">Vídeo</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="91"/>
+        <source>Development</source>
+        <translation type="unfinished">Desenvolvimento</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="94"/>
+        <source>Image</source>
+        <translation type="unfinished">imagem</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="97"/>
+        <source>Game</source>
+        <translation type="unfinished">Jogos</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="100"/>
+        <source>Office</source>
+        <translation type="unfinished">Escritorio</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="103"/>
+        <source>Education</source>
+        <translation type="unfinished">Educação</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="106"/>
+        <source>System</source>
+        <translation type="unfinished">Sistema</translation>
+    </message>
+    <message>
+        <location filename="../src/UtilityFunction/splitbarframe.cpp" line="109"/>
+        <source>Others</source>
+        <translation type="unfinished">Outros</translation>
+    </message>
+</context>
+</TS>
